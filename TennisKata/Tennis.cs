0379e66f5e78d89f94be5cb292c5b5951653626e
Robbin.Hsu <robbin.hsu@collaborate.tw>
2019-10-18 11:16:07 +0800

    Refactor Tennis

--- TennisKata/Tennis.cs
+++ TennisKata/Tennis.cs
@@ -1,18 +1,22 @@
-﻿namespace TennisKata
+﻿using System.Collections.Generic;
+
+namespace TennisKata
 {
     public class Tennis
     {
         private int _firstPlayerScore;
 
+        private Dictionary<int, string> _scoreLookup = new Dictionary<int, string>
+        {
+            {1,"Fifteen" },
+            {2,"Thirty" },
+        };
+
         public string Score()
         {
-            if (_firstPlayerScore == 1)
+            if (_firstPlayerScore == 1 || _firstPlayerScore == 2)
             {
-                return "Fifteen Love";
-            }
-            if (_firstPlayerScore == 2)
-            {
-                return "Thirty Love";
+                return $"{_scoreLookup[_firstPlayerScore]} Love";
             }
 
             return "Love All";
