da13cab731984dfc2201f53030e7aff0e59105a3
Robbin.Hsu <robbin.hsu@collaborate.tw>
2019-10-18 11:01:22 +0800

    Add Love All Test

--- TennisKata/TennisTest.cs
+++ TennisKata/TennisTest.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace TennisKata
 {
@@ -7,8 +6,10 @@ namespace TennisKata
     public class TennisTest
     {
         [TestMethod]
-        public void TestMethod1()
+        public void Love_All()
         {
+            var tennis = new Tennis();
+            Assert.AreEqual("Love All", tennis.Score());
         }
     }
 }
